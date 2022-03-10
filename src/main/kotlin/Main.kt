c8d658a7dbdbfbf9f37280f96424c8d7b2ef8772
Gustavo Paes Crescêncio <gustavo.paes1414@gmail.com>
2022-03-09 23:27:01 -0300

    Projeto finalizado.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,21 @@
 fun main(){
 val tim = Player("Tim",1,0,0)
-//    var sword = Weapon("Sword", 10)
-//    tim.weapon = sword
-//    tim.show()
-//    val louise = Player("Louise",1,1,34)
-//    louise.weapon = tim.weapon
-//    louise.show()
-//    println("${louise.name}")
-//    tim.show()
-//var spear = Weapon("Spear",11)
-//tim.weapon = spear
-//    println("${spear.name}")
-//    tim.show()
-//    spear.damageInflicted = 15
-//    tim.show()
+    var sword = Weapon("Sword", 10)
+    tim.weapon = sword
+    tim.show()
+    val louise = Player("Louise",1,1,34)
+    louise.weapon = tim.weapon
+    louise.show()
+    println("${louise.name}")
+    tim.show()
+var spear = Weapon("Spear",11)
+tim.weapon = spear
+    println("${spear.name}")
+    tim.show()
+    spear.damageInflicted = 15
+    tim.show()
     val healthPotion = Loot("Health Potion", LootType.POTION, 10.0)
-//   tim.inventory.add(healthPotion)
-//tim.showInventory()
-//println(tim)
-    tim.inventory.add(healthPotion)
-    tim.showInventory()
-    println(tim.toString())
+   tim.inventory.add(healthPotion)
+tim.showInventory()
+println(tim)
 }
